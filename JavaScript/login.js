c7944482bd5681df6b9169da8a2c4a03f7f4eb5d
MESA.RIN <MESA.RIN@PNC.LOCAL>
2026-01-28 19:35:22 +0700

    fix code

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -1,16 +1,22 @@
 function login() {
     let user = document.getElementById("username").value;
     let pass = document.getElementById("password").value;
+    let errorElement = document.getElementById("error");
+
+    if (user === "" || pass === "") {
+        errorElement.innerText = "Please enter both username and password!";
+        return;
+    }
 
     if (user === "admin" && pass === "admin123") {
         localStorage.setItem("role", "admin");
-        window.location.href = "admin.html";
+        window.location.href = "HTML/home.html";
     }
     else if (user === "user" && pass === "user123") {
         localStorage.setItem("role", "user");
-        window.location.href = "user.html";
+        window.location.href = "HTML/home.html";
     }
     else {
-        document.getElementById("error").innerText = "Invalid login!";
+        errorElement.innerText = "Invalid login!";
     }
 }
